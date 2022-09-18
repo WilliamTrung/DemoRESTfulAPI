0d6dca3f4f4aeda53eaa3137e5756fcb1e46bf3f
WilliamTrung <williamthanhtrungq2@gmail.com>
2022-09-18 16:24:30 +0700

    Rework Demo API

--- DemoAPI/Program.cs
+++ DemoAPI/Program.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Mapping;
 using Mapping.IRepository;
 using Mapping.Repository;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,20 +18,40 @@ builder.Services.AddMvc();
 
 Console.WriteLine("Add automapper");
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AutomaticAuthentication = false;
+});
 using (var config = builder.Configuration)
 {
     string connectionString = config.GetConnectionString("DefaultConnection");
     builder.Services.AddDbContext<DemoContext>(options => options.UseSqlServer(connectionString));
     builder.Services.AddAutoMapper(typeof(Mapping.Mapping));
 }
+//logging
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestHeaders.Add("sec-ch-ua");
+    logging.ResponseHeaders.Add("MyResponseHeader");
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
 
+});
 
 var app = builder.Build();
+//enable app logging
+app.UseHttpLogging();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+        c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "Test API v1");
+    });
 }
 
 app.UseHttpsRedirection();
